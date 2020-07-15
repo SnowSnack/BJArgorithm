c5b50235f6823f845b44afb7b96938f5694c6586
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2020-07-15 17:12:34 +0900

    ArraytoString? 2908

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,58 +11,16 @@ class Program
 {
     public void Solution()
     {
-        string input        = Console.ReadLine();
-        string upperInput   = input.ToUpper();
+        string[] inputs = Console.ReadLine().Split(' ');
 
-        Dictionary<char, int> upperDictionary = new Dictionary<char, int>();
+        List<int> list = new List<int>();
 
-        List<char>  keys;
-        List<int>   values;
-        char        answer;
-        int         answerIndex;
-
-        for(int i=0; i<upperInput.Length; i++)
+        for(int i=0;i<inputs.Length; i++)
         {
-            char toAddKey = upperInput[i];
-
-            if (upperDictionary.ContainsKey(toAddKey))
-                upperDictionary[toAddKey]++;
-            else
-                upperDictionary.Add(toAddKey, 1);
-        }
-        
-        keys = upperDictionary.Keys.ToList<char>();
-        values = upperDictionary.Values.ToList<int>();
-        answerIndex = 0;
-
-        if (keys.Count > 0)
-        {
-            for (int i = 1; i < keys.Count; i++)
-            {
-                if (upperDictionary[keys[i]] > upperDictionary[keys[i - 1]])
-                    answerIndex = i;
-                else
-                    answerIndex = i - 1;
-            }
-        }
-        else
-        {
-            answerIndex = 0;
+            inputs[i].ToArray().Reverse().ToArray();
         }
 
-        answer = keys[answerIndex];
-        
-        for (int i = 0; i < values.Count; i++)
-        {
-            if (!i.Equals(answerIndex))
-            {
-                if (values[answerIndex].Equals(values[i]))
-                {
-                    answer = '?';
-                    break;
-                }
-            }
-        }
+        int answer = inputs.Length;
 
         Console.WriteLine(answer);
     }
