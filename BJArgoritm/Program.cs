5d938797585eda2baef2ffb5e831cb16683134e1
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2020-08-08 23:09:46 +0900

    소수구하기 재현실패..

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -1,44 +1,11 @@
 ﻿using System;
 using static System.Console;
-
+using System.Linq;
 class Program
 {
     public void Solution()
     {
-        int countInput = int.Parse(ReadLine());
-
-        double remain;
-        int x=0, y =0;
-
-        for (int j=0; j<countInput; j++)
-        {
-            string[] inputs = ReadLine().Split(' ');
-
-            if (inputs.Length < 2)
-                return;
-            WriteLine("lenth" + inputs.Length);
-            for (int i = 0; i < inputs.Length; i++)
-            {
-                if(i.Equals(0))
-                    x = int.Parse(inputs[i]);
-                else
-                    y = int.Parse(inputs[i]);
-            }
-
-            for (int i = 1; i < i + 1; i++)
-            {
-                if (i * i >= y - x)
-                {
-                    if ((--i).Equals(0))
-                    {
-                        WriteLine(1);
-                    }
-                    remain = Math.Ceiling((double)(y-x - i * i)/i);
-                    WriteLine(i*2-1+remain);
-                    break;
-                }
-            }              
-        }
+        
     }
     public static void Main(string[] args)
     {
