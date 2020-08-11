172f5a9f2f33713f605747309b6789106151c007
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2020-08-11 16:23:12 +0900

    2447ing

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -1,17 +1,62 @@
 ﻿using System;
 using static System.Console;
-using System.Text;
+
 class Program
 {
-    static void Main(string[] args)
+    public int Factorial(short _num)
     {
-        StringBuilder sb = new StringBuilder();
-        WriteLine(StarAction(short.Parse(ReadLine()), ref sb));
-        return;
+        if (_num > 0)
+            return Factorial((short) (_num - 1)) * _num;
+        return 1;
     }
-    static StringBuilder StarAction(short _count, ref StringBuilder _sb)
+    public int Fivonacii(short _num)
     {
+        if (_num > 1)
+        {
+            return Fivonacii((short) (_num - 1)) + Fivonacii((short) (_num - 2));
+        }
+        return _num;
+    }
 
-        return _sb;
+    void solve(ref char[,] _mat, int _y, int _x, int _num)
+    {
+        if (_num.Equals(1))
+        {
+            WriteLine("num = " + _num + " mat[{0}][{1}]", _y, _x);
+            _mat[_y, _x] = '*';
+            return;
+        }
+
+        int divTmp = _num / 3;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (i == 1 && j == 1)
+                {
+                    WriteLine("in if = num = " + _num + " mat[{0}][{1}]", _y + (i * divTmp), _x + (j * divTmp));
+                }
+                else
+                    solve(ref _mat, _y + (i * divTmp), _x + (j * divTmp), divTmp);
+            }
+        }
     }
-}
+
+
+    public void Solution()
+    {
+        short input = short.Parse(ReadLine());
+        char[,] mat = new char[2000, 2000];
+        solve(ref mat, 0, 0, input);
+
+
+        for (int i = 0; i < input; i++)
+        {
+            for (int j = 0; j < input; j++)
+            {
+                Write(mat[i, j]);
+            }
+            WriteLine();
+        }
+
+    }
