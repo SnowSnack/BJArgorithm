424f9f55e00b7f8361bab28be7a2011dcbdcf535
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2020-08-27 16:31:23 +0900

    퀵소트 알앤디

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -1,39 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 class Program
 {
-    void PartSort(List<int> _nums, int _left, int _high, int _pivot)
+    void PartSort(List<int> _nums, int _left, int _right)
     {
-        while (true)
+        if(_left<_right)
         {
-            if (_nums[_left] >= _nums[_high])
-            {
-                int tmp = _nums[_left];
-                _nums[_left] = _nums[_high];
-                _nums[_high] = tmp;
-            }
-            else
-            {
+            int pivot = _nums[(_left + _right) / 2];
+            int high = _right;
+            int low = _left;
+            int tmp;
 
+            while (low < high)
+            {
+                while (_nums[low] < pivot && low < high)
+                    low++;
+                while (_nums[high] > pivot && high > low)
+                    high--;
+
+                tmp = _nums[low];
+                _nums[low] = _nums[high];
+                _nums[high] = tmp;
             }
-            _left++; _high--;
-            if (_left >= _high)
-                break;
+
+            PartSort(_nums, _left, high - 1);
+            PartSort(_nums, high + 1, _right);
+
         }
-        List<int> lefList = new List<int>();
-        //_nums.CopyTo()
+
+    }
+
+    public void quickSort(List<int> arr, int left, int right)
+    {
+        int i, j, pivot, tmp;
+        if (left < right)
+        {
+            i = left; j = right;
+            pivot = arr[(left + right) / 2];
+            //분할 과정
+            while (i < j)
+            {
+                while (arr[j] > pivot) j--;
+                // 이 부분에서 arr[j-1]에 접근해서 익셉션 발생가능함.
+                while (i < j && arr[i] < pivot) i++;
+
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+            //정렬 과정
+            quickSort(arr, left, i - 1);
+            quickSort(arr, i + 1, right);
+        }
     }
 
     void Solution()
     {
-        int pivot;
         int left, right, low, high;
 
         int count = int.Parse(ReadLine());
-
-        List<int> a = new List<int>();
 
         List<int> nums = new List<int>();
         
@@ -42,18 +70,21 @@ class Program
             nums.Add(int.Parse(ReadLine()));
         }
 
-        pivot = 0;
-
         left = 0;
 
-        right = nums.Count;
+        right = nums.Count-1;
 
         low = left+1;
 
         high = right;
 
-        PartSort(nums, left, high, pivot);
+        PartSort(nums, left, right);
 
+        for (int i = 0; i < nums.Count; i++)
+        {
+            Write(nums[i]);
+        }
+        ReadLine();
     }
 
     static void Main(string[] args)
