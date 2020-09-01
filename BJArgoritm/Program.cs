16e6d7cc4735b22f04c1fb36dcee831256d19115
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2020-09-01 16:42:02 +0900

    CountingSort Trying

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -1,62 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿
 using System.Linq;
-using System.Text;
 using static System.Console;
-
+using System.Text;
 class Program
 {
-    public void PartSort(int[] _arr, int _left, int _right)
+    public int[] CountSort(int[] _arr)
     {
-        if (_left < _right)
+        int[] sum = Enumerable.Repeat(0,_arr.Max()+1).ToArray();
+        int[] cumulSum = new int[_arr.Max()+1];
+        int[] toReturn = new int[_arr.Length];
+        for (int i=1; i<_arr.Length; i++)
         {
-            int tmp;
-            int low = _left;
-            int high = _right;
-            int pivot = _arr[(_left + _right) / 2];
-            while (low < high)
-            {
-                while (_arr[low] < pivot) low++;
-                while (_arr[high] > pivot && low < high) high--;
-
-                tmp = _arr[low];
-                _arr[low] = _arr[high];
-                _arr[high] = tmp;
-            }
-
-            PartSort(_arr, _left, high - 1);
-            PartSort(_arr, high + 1, _right);
+            sum[_arr[i]]++;
         }
-    }
 
+        cumulSum[0] = sum[0];
+        for(int i=1; i<=_arr.Max(); i++)
+        {
+            cumulSum[i] = cumulSum[i-1]+ sum[i];
+        }
+        for (int i = _arr.Length-1; i >= 1; i--)
+        {
+            toReturn[cumulSum[_arr[i]]] = _arr[i];
+            cumulSum[_arr[i]]--;
+        }
+
+        WriteLine();
+
+        return toReturn;
+    }
 
     void Solution()
     {
-        int left, right;
-
-        int count = int.Parse(ReadLine());
-
-        int[] nums = new int[count];
+        int cnt = int.Parse(ReadLine());
 
         StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<count; i++)
+        int[] arr = new int[cnt+1];
+        for (int i = 1; i <= cnt; i++)
         {
-            nums[i] = int.Parse(ReadLine());
+            arr[i] = int.Parse(ReadLine());
         }
 
-        left = 0;
+        int[] answers =  CountSort(arr);
 
-        right = nums.Length-1;
-
-        PartSort(nums, left, right);
-
-        for (int i = 0; i < nums.Length; i++)
+        for(int i=1; i<answers.Length; i++)
         {
-            sb.AppendFormat("{0}\n",nums[i]);
+            if(!answers[i].Equals(0))
+                sb.AppendFormat("{0}\n",
+            answers[i]);
         }
-
         Write(sb);
     }
 
