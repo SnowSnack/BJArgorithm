8f207d8458d3dc1d24a67c22306f0134bd185884
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2020-09-28 17:15:18 +0900

    11650

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -4,41 +4,102 @@ using System.Linq;
 using static System.Console;
 class Program
 {
-    static void Swap(int[,] _arr, int beforeIndex, int foreIndex)
+    public void Swap(int[,] _arr, int _left,int _right, int _tmp1, int _tmp2)
     {
-        var tmp = _arr[beforeIndex, 0];
-        var tmp2 = _arr[beforeIndex, 1];
-        _arr[beforeIndex, 0] = _arr[foreIndex, 0];
-        _arr[beforeIndex, 1] = _arr[foreIndex, 1];
-        _arr[foreIndex, 0] = tmp;
-        _arr[foreIndex, 1] = tmp;
+        _tmp1 = _arr[_left,0];
+        _tmp2 = _arr[_left, 1];
+        _arr[_left, 0] = _arr[_right, 0];
+        _arr[_left, 1] = _arr[_right, 1];
+        _arr[_right, 0] = _tmp1;
+        _arr[_right, 1] = _tmp2;
+        WriteLine("[{0} , {1}] [{2} , {3}]", _arr[_left, 0], _arr[_left, 1], _arr[_right, 0], _arr[_right, 1]);
     }
 
-    public int Partition(int[,] _arr, int _left, int _right)
+    int Partition(int[,] _arr, int _left, int _right, int _secIndex)
     {
-        int pivot = _left;
-        for (int i = _left; i < _right; i++)
+        WriteLine(_secIndex);
+        int pivot, tmp1=0, tmp2=0;
+        int low, high;
+
+        low = _left;
+        high = _right + 1;
+        pivot = _arr[_left,_secIndex]; // 정렬할 리스트의 가장 왼쪽 데이터를 피벗으로 선택(임의의 값을 피벗으로 선택)
+
+        /* low와 high가 교차할 때까지 반복(low<high) */
+        do
         {
-            if (_arr[i,0] <= _arr[_right,0])
+            /* list[low]가 피벗보다 작으면 계속 low를 증가 */
+            do
             {
-                Swap(_arr, pivot, i);
-                pivot++;
+                low++; // low는 left+1 에서 시작
+            } while (low <= _right && _arr[low,_secIndex] < pivot);
+
+            /* list[high]가 피벗보다 크면 계속 high를 감소 */
+            do
+            {
+                high--; //high는 right 에서 시작
+            } while (high >= _left && _arr[high,_secIndex] > pivot);
+
+            // 만약 low와 high가 교차하지 않았으면 list[low]를 list[high] 교환
+            if (low < high)
+            {
+                Swap(_arr, low, high, tmp1, tmp2);
             }
-        }
-        Swap(_arr, pivot, _right);
-        return pivot;
+        } while (low < high);
+        Swap(_arr, _left, high, tmp1, tmp2);
+
+        // 피벗의 위치인 high를 반환
+        return high;
     }
 
-    public void QuickSort(int[,] _arr, int _left, int _right)
+    int PartitionKey(int[,] _arr, int _left, int _right)
     {
+        int pivot, tmp1 = 0, tmp2 = 0;
+        int low, high;
+
+        low = _left;
+        high = _right + 1;
+        pivot = _arr[_left, 1]; // 정렬할 리스트의 가장 왼쪽 데이터를 피벗으로 선택(임의의 값을 피벗으로 선택)
+
+        /* low와 high가 교차할 때까지 반복(low<high) */
+        do
+        {
+            /* list[low]가 피벗보다 작으면 계속 low를 증가 */
+            do
+            {
+                low++; // low는 left+1 에서 시작
+            } while (low <= _right && _arr[low, 1] < pivot);
+
+            /* list[high]가 피벗보다 크면 계속 high를 감소 */
+            do
+            {
+                high--; //high는 right 에서 시작
+            } while (high >= _left && _arr[high, 1] > pivot);
+
+            // 만약 low와 high가 교차하지 않았으면 list[low]를 list[high] 교환
+            if (low < high)
+            {
+                Swap(_arr, low, high, tmp1, tmp2);
+            }
+        } while (low < high);
+        Swap(_arr, _left, high, tmp1, tmp2);
+
+        // 피벗의 위치인 high를 반환
+        return high;
+    }
+
+    public void QuickSort(int[,] _arr, int _left, int _right, int _secIndex)
+    {
+        int pivot = Partition(_arr, _left, _right, _secIndex);
+
         if (_left < _right)
         {
-            int pivot = Partition(_arr, _left, _right);
-            QuickSort(_arr, _left, pivot - 1);
-            QuickSort(_arr, pivot + 1, _right);
+            QuickSort(_arr, _left, pivot - 1, _secIndex);
+            QuickSort(_arr, pivot, _right, _secIndex);
         }
+        int keyLeft = _left;
     }
-
+     
     void Solution()
     {
         int cnt = int.Parse(ReadLine());
@@ -52,8 +113,8 @@ class Program
             pairs[i, 1] = arr[1];
         }
 
-        QuickSort(pairs, 0, cnt-1);
-
+        QuickSort(pairs, 0, cnt - 1, 0);
+        QuickSort(pairs, 0, cnt - 1, 1);
         for(int i=0; i<cnt; i++)
         {
             WriteLine("{0} {1}", pairs[i, 0], pairs[i, 1]);
@@ -63,6 +124,7 @@ class Program
     static void Main(string[] args)
     {
         Program program = new Program();
+
         program.Solution();
     }
 }
