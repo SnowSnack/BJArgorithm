fb958a6557215441f9abd8530f68149d16b519aa
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2020-10-19 18:00:15 +0900

    15649

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -6,35 +6,55 @@ using System.Collections.Generic;
 
 class Program
 {
-    public static void dfs(int[][] _a, bool[] _c, int _v, bool _flag, StringBuilder _sb)
+    public static void dfs2(bool[,] _array, bool[] _visited, int _startVertex,int _len, StringBuilder _sb)
+    {
+        _visited[_startVertex] = true;
+        _sb.Append(_startVertex + " ");
+        for (int i = 1; i < _len; i++)
+        {
+            if (_array[_startVertex,i].Equals(true) && !_visited[i])
+            {
+                dfs2(_array, _visited, i,_len, _sb);
+            }
+        }
+    }
+
+    public static void dfs(bool[,] _array, bool[] _visited, int _startVertex, int _len, bool _flag, StringBuilder _sb)
     {
         Stack<int> stack = new Stack<int>();
-        int n = _a.Length - 1;
 
-        stack.Push(_v);
-        _c[_v] = true;
-        _sb.Append(_v + " ");
+        stack.Push(_startVertex);
+        _visited[_startVertex] = true;
+        _sb.Append(_startVertex + " ");
 
-        while (!stack.Equals(default))
+        while (!(stack.Count==0))
         {
-            int vv = stack.Peek();
+            int root = stack.Peek();
             _flag = false;
-            for (int i = 1; i <= n; i++)
+
+            for (int i = 1; i < _len; i++)
             {
-                if (_a[vv][i] == 1 && !_c[i])
+                if (_array[root,i].Equals(true) && !_visited[i])
                 {
+                    _sb.AppendFormat("{0} {1} {2}",root, i,_array[root,i]);
                     stack.Push(i);
                     _sb.Append(i + " ");
-                    _c[i] = true;
+                    _visited[i] = true;
                     _flag = true;
                     break;
                 }
             }
+            
             if (!_flag)
             {
                 stack.Pop();
             }
         }
+    }
+
+    public void MyDFS(ref int _len, StringBuilder _sb)
+    {
+            
     }
 
     public void Solution(int _num,int _cnt, int _prevNum, StringBuilder _sb)
@@ -75,9 +95,29 @@ class Program
         Console.SetOut(sw);
 
         int[] inputs = sr.ReadLine().Split().Select(a => int.Parse(a)).ToArray();
+        //int first = 10, s = 7;
+        int toLength = 0;
+        
+        //int[] toOutput = new int[first];
+
+        //for(int i=0; i<first; i++)
+        //{
+        //    toOutput[i] = 0;
+        //}
+
+        //bool[] b = new bool[first];
+        bool flag = false;
+        //dfs2(toOutput, b, 1, first, sb);
+        
+        
 
         Program pg = new Program();
-        pg.Solution(inputs[0], inputs[1], -1, sb);
+        for(int i=0; i<inputs[0]; i++)
+        {
+            pg.MyDFS(ref toLength, sb);
+        }
+        
+        //pg.Solution(inputs[0], inputs[1], -1, sb);
 
         sw.Write(sb);
     }
