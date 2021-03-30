65047e3b00f29d674a7c71dee7fddae268fe476d
SnowSnack <50358365+SnowSnack@users.noreply.github.com>
2021-03-30 17:59:25 +0900

    dfs 탐구..

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -5,22 +5,61 @@ using System.IO;
 
 class Program
 {
+    int max, cnt;
+    bool[] visited;
+    int[] nums;
+    StringBuilder sb;
+    public void dfs(int _level)
+    {
+        if(_level.Equals(cnt))
+        {
+            for (int i = 0; i < cnt; i++)
+            {
+                sb.Append(nums[i] + " ");
+            }
+            sb.AppendLine();
+        }
+        else
+        {
+            for(int i=1; i<=max; i++)
+            {
+                if (visited[i])
+                    continue;
+
+                visited[i] = true;
+                nums[_level] = i;
+                dfs(_level + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
     public void Solution()
     {
-        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
-        StringBuilder sb = new StringBuilder();
+        StreamReader sr = new StreamReader(Console.OpenStandardInput());
+        StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+        sb = new StringBuilder();
 
-        int[] inputs = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+        int[] inputs = sr.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+        max = inputs[0];
+        cnt = inputs[1];
 
-        for(int i=0; i<inputs[1]; i++)
+        visited= new bool[max + 1];
+        nums= new int[cnt];
+
+        for(int i=1; i<=max; i++)
         {
-
+            visited[i] = true;
+            nums[0] = i;
+            dfs(1);
+            visited[i] = false;
         }
 
-        sb.Length--;
-        sw.WriteLine(sb);
-        sb.Clear();
+        sw.WriteLine(sb.ToString());
         sw.Close();
+        sr.Close();
+        sb.Clear();
+
     }
 
     static void Main(string[] args)
