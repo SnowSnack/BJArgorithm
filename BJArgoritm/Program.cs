734f6cad698b30da610b2305d3ed4b9eb22a901d
SnowSnack <crazy9268@gmail.com>
2021-03-31 20:30:31 +0900

    BJ15652 하는중..

--- BJArgoritm/Program.cs
+++ BJArgoritm/Program.cs
@@ -6,30 +6,38 @@ using System.IO;
 class Program
 {
     int max, cnt;
+    bool[] fVisited;
     bool[] visited;
     int[] nums;
     StringBuilder sb;
     public void dfs(int _level)
     {
-        if(_level.Equals(cnt))
+        if (_level.Equals(cnt))
         {
             for (int i = 0; i < cnt; i++)
             {
-                sb.Append(nums[i] + " ");
+                sb.Append(nums[i]);
+                sb.Append(' ');
             }
-            sb.AppendLine();
+            sb.Append('\n');
         }
         else
         {
-            for(int i=1; i<=max; i++)
+            for (int i = 1; i <= max; i++)
             {
                 if (visited[i])
+                {
                     continue;
+                }
+                //Console.WriteLine("level = " + _level + ", cnt-1 = " + (cnt-1));
+                //if(_level.Equals(cnt-1))
+                //    Console.WriteLine("nums["+ _level +"] = " + nums[_level] + '\n'+ "nums[0] = " + nums[0] + '\n'+ nums[_level].Equals(nums[0]) + '\n' + '\n');
+                
+                if (i.Equals(nums[_level-1]))
+                    visited[i] = true;
 
-                visited[i] = true;
                 nums[_level] = i;
                 dfs(_level + 1);
-                visited[i] = false;
             }
         }
     }
@@ -44,15 +52,19 @@ class Program
         max = inputs[0];
         cnt = inputs[1];
 
+        fVisited = new bool[max + 1];
         visited= new bool[max + 1];
         nums= new int[cnt];
 
         for(int i=1; i<=max; i++)
         {
-            visited[i] = true;
+            visited = new bool [max+1];
+            for(int j=0; j<i; j++)
+            {
+                visited[j] = true;
+            }
             nums[0] = i;
             dfs(1);
-            visited[i] = false;
         }
 
         sw.WriteLine(sb.ToString());
